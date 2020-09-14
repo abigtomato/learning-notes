aae596652ffd315904de0b35f4e3b616c96eb6a8
abigtomato <45549394+abigtomato@users.noreply.github.com>
2020-09-14 23:31:23 +0800

    tj

--- code-jdk/src/main/java/com/abigtomato/jdk/_RefType_ThreadLocal/T03_WeakReference.java
+++ code-jdk/src/main/java/com/abigtomato/jdk/_RefType_ThreadLocal/T03_WeakReference.java
@@ -7,5 +7,13 @@ public class T03_WeakReference {
     public static void main(String[] args) {
         // 弱引用
         WeakReference<M> m = new WeakReference<>(new M());
+
+        System.out.println(m.get());
+        System.gc();
+        System.out.println(m.get());
+
+        ThreadLocal<M> tl = new ThreadLocal<>();
+        tl.set(new M());
+        tl.remove();
     }
 }
